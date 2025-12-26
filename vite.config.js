56de89951936554f04d60da20e166c6e9f482723
“Sorri” <37029743+ApolloG01@users.noreply.github.com>
2025-12-26 10:40:19 +0100

    Update names to match GitHub repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/shopping-cart-project/',
+  base: '/Project-Shopping-Cart---Ether-The-Void-Market/',
 });
